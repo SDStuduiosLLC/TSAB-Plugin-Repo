4f4d2f7559030ff7382e3aed66f7bec362ce584b
WhizBangPop <101482788+whizbangpop@users.noreply.github.com>
2022-11-13 13:01:20 +0000

    Create templatePlugin.js

---
+++ plugins/@sdstudios/templatePlugin.js
@@ -0,0 +1,27 @@
+ import { SlashCommandBuilder, CommandInteraction, Client, EmbedBuilder } from "discord.js";
+import { webhookReporter } from "../utilities";
+
+module.exports = {
+    pluginInfo: {
+        /** Name of the plugin. Simple as it gets really. */
+        name: "",
+        /** Author(s) of the plugin. Please use the GitHub username of the person creating the PR on the repo */
+        author: "",
+        /** Current version of the plugin. Please change this as you update as it allows the CLI to know when a new plugin is waiting. */
+        version: "",
+        /** If present, the plugin will be imported only if the checksum matches. */
+        md5hash: "",
+    },
+    data: new SlashCommandBuilder()
+        .setName('plugin-name')
+        .setDescription('This is a template plugin - love the TSAB team :>'),
+    async execute(ctx: CommandInteraction, client: Client) {
+        const embed = new EmbedBuilder()
+            .setTitle('TSAB Template Plugin')
+            .setDescription('If you are a plugin developer, and have imported the template, it worked! If you are a regular user, run `/info` and contact the user or role mentioned in that embed.')
+
+        await ctx.reply({embeds: [embed]})
+
+        webhookReporter('note', 'Template plugin executed successfully.')
+    }
+}
